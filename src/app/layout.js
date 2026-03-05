52c481a9a1a919d7708fe3cab577e2a71c71cca1
Hasan Siddiqui <hasan@hayaland.com>
2026-03-06 07:25:24 +0900

    Fix viewport warnings

--- src/app/layout.js
+++ src/app/layout.js
@@ -16,11 +16,12 @@ export const metadata = {
     type: 'website',
     locale: 'en_US',
   },
-  viewport: {
-    width: 'device-width',
-    initialScale: 1,
-    maximumScale: 5,
-  },
+};
+
+export const viewport = {
+  width: 'device-width',
+  initialScale: 1,
+  maximumScale: 5,
 };
 
 export default function RootLayout({ children }) {
