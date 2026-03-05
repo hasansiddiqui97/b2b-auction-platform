51e321307df66da19a775be69664f20172be0894
Hasan Siddiqui <hasan@hayaland.com>
2026-03-06 07:20:57 +0900

    Add test page

---
+++ src/app/test/page.js
@@ -0,0 +1,10 @@
+'use client';
+
+export default function TestPage() {
+  return (
+    <div style={{ padding: '50px', textAlign: 'center' }}>
+      <h1>HW Test Page</h1>
+      <p>If you see this, the app is working!</p>
+    </div>
+  );
+}
